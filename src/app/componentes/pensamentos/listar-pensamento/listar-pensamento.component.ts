32f1b9fdf2cd7563b795c32e6dac032475d65592
Gustavoksbr <gustavosalesi@hotmail.com>
2024-10-21 16:38:13 -0300

    ngif

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PensamentoComponent } from "../pensamento/pensamento.component";
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
-  imports: [RouterLink, PensamentoComponent,NgFor],
+  imports: [RouterLink, PensamentoComponent,NgFor,NgIf],
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
